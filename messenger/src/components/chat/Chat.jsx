b09a4ae38cf6d2d10e843b6708fe96ec130f7524
Gustavo Poeta <gustavpoeta@gmail.com>
2024-07-29 11:25:39 -0300

    add auto scroll to bottom when there's a new message, text wrap to messages and time that the message was sent

--- messenger/src/components/chat/Chat.jsx
+++ messenger/src/components/chat/Chat.jsx
@@ -1,101 +1,148 @@
-import './Chat.css';
-import { useState, useEffect, useRef } from 'react';
-import users from '../../assets/users.json';
-import PropTypes from 'prop-types';
-import insertMediaIcon from '../../assets/imageForInputFile.svg';
-import emojiButton from '../../assets/emoji-sunglasses-fill.svg';
-import Message from './message/Message.jsx'
- 
-function Chat (props) {
+import "./Chat.css";
+import { useState, useEffect, useRef } from "react";
+import users from "../../assets/users.json";
+import PropTypes from "prop-types";
+import insertMediaIcon from "../../assets/imageForInputFile.svg";
+import emojiButton from "../../assets/emoji-sunglasses-fill.svg";
+import Message from "./message/Message.jsx";
 
-    const [arrayOfMessages, setArrayMessage] = useState([]);
+function Chat(props) {
+  const messagesDiv = useRef(null);
+  const messageRef = useRef(null);
+  const inputRef = useRef(null);
 
-    const headerHandler = () => {
-        const matchedUser = users.users.find(user => user.name === props.userClicked);
+  const [arrayOfMessages, setArrayMessage] = useState([]);
 
-        if (matchedUser) {
-            return (
-                <>
-                    <img src={matchedUser.profile_picture} alt="icone de perfil" id='pfp' />
-                    <h5 id='userName'>{matchedUser.name}</h5>
-                </>
-            );
+  const headerHandler = () => {
+    const matchedUser = users.users.find(
+      (user) => user.name === props.userClicked
+    );
+
+    if (matchedUser) {
+      return (
+        <>
+          <img
+            src={matchedUser.profile_picture}
+            alt="icone de perfil"
+            id="pfp"
+          />
+          <h5 id="userName">{matchedUser.name}</h5>
+        </>
+      );
+    }
+  };
+
+  // if the keyTyped prop isn't empty it makes the input focus
+  useEffect(() => {
+    if (props.keyTyped != "") {
+      inputRef.current.focus();
+    }
+  }, [props.keyTyped]);
+
+  // checks if enter is pressed and if yes adds the input's value to the array of messages
+  const enterTyped = (event) => {
+    if (event.key == "Enter") {
+      const newMessage = inputRef.current.value;
+
+      if (newMessage.trim()) {
+        const currentTime = getDate();
+
+        setArrayMessage((prev) => [
+          ...prev,
+          { content: newMessage, owner: "Self", time: currentTime },
+        ]);
+      }
+
+      inputRef.current.value = "";
+    }
+  };
+
+  // function that scrolls the messageDiv to view the lastest message
+  const scrollToBottom = () => {
+    messageRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  // observe if there is a new message 
+  const observerConfig = { attributes: false, childList: true, subtree: false };
+  
+  const mutationFn = (mutationList) => {
+
+    for (const mutation of mutationList) {
+
+        // if it observes a new child node and its className corresponds to messageSelf it scrolls down to bottom
+      if (mutation.type === "childList" && mutation.target.lastChild.children[0].className === "messageContainer messageSelf") {
+            scrollToBottom();
         }
     }
 
-    const inputRef = useRef(null);
+  };
 
-    // if the keyTyped prop isn't empty it makes the input focus
-    useEffect(() => {
-        if (props.keyTyped != "") {
-            inputRef.current.focus();
-        } 
-    }, [props.keyTyped]);
+  const observer = new MutationObserver(mutationFn);
 
+  // if messagesDiv ref isnt null it starts to observe
+  if (messagesDiv.current != null) {
+    observer.observe(messagesDiv.current, observerConfig);
+  }
 
-    // checks if enter is pressed and if yes adds the input's value to the array of messages
-    const enterTyped = (event) => {
-        if (event.key == "Enter") {
+  const getDate = () => {
+    const date = new Date();
+    const hour = date.getHours().toString().padStart(2, '0'); // Format hour to 2 digits
+    const minute = date.getMinutes().toString().padStart(2, '0'); // Format minute to 2 digits
 
-            const newMessage = inputRef.current.value;
+    return `${hour}:${minute}`;
+}
 
-            if (newMessage.trim()) {
-                setArrayMessage((prev) => [...prev, {content: newMessage, owner: "Self"}] )
-            }
+  return (
+    <>
+      <div id="Chat">
+        <header>
+          <div id="headerInfoContainer">{headerHandler()}</div>
+        </header>
 
-            inputRef.current.value = '';
-        } 
-    }
+        <div id="messages" ref={messagesDiv}>
+          {arrayOfMessages.map((message, index) => {
+            return (
+              // calls a map into arrayOfMessages which iterates and calls the message component for each message in the array
+              <Message
+                key={index}
+                messageOwner={message.owner}
+                messageContent={message.content}
+                ref={messageRef}
+                messageDate = {message.time}
+              />
+            );
+          })}
+        </div>
 
+        <div id="inputMessage">
+          <div id="otherInputsContainer">
+            <button id="emoteBtn">
+              <img
+                src={emojiButton}
+                alt="icon for sending emojis"
+                id="emoteBtnIcon"
+              />
+            </button>
 
+            <button id="inputMedia">
+              <img
+                src={insertMediaIcon}
+                alt="icon for inserting media"
+                id="inputMediaIcon"
+              />
+            </button>
+          </div>
 
-    return (
-        <>
-        
-            <div id='Chat'>
-                
-                <header>
-                    <div id='headerInfoContainer'>
-                        {
-                            headerHandler()
-                        }
-                    </div>
-                </header>
-
-                <div id="messages">
-                    {arrayOfMessages.map((message, index) => {
-                        return (
-                            // calls a map into arrayOfMessages which iterates and calls the message component for each message in the array
-                            <Message key = {index} messageOwner = {message.owner} messageContent = {message.content}/>
-                        );
-                    })}
-                </div>
-                
-                <div id="inputMessage">
-
-                    <div id='otherInputsContainer'>
-
-                        <button id='emoteBtn'>
-                            <img src={emojiButton} alt="icon for sending emojis" id='emoteBtnIcon' />
-                        </button>
-
-                        <button id='inputMedia'><img src={insertMediaIcon} alt="icon for inserting media" id='inputMediaIcon'/></button>
-                        
-                    </div>
-
-                    <input type="text" id='input' ref={inputRef} onKeyDown={enterTyped}/>
-
-                </div>
-            </div>
-
-        </>
-    )
-
+          <input type="text" id="input" ref={inputRef} onKeyDown={enterTyped} maxLength="500" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 Chat.propTypes = {
-    userClicked: PropTypes.string.isRequired,
-    keyTyped: PropTypes.string
-}
+  userClicked: PropTypes.string.isRequired,
+  keyTyped: PropTypes.string,
+};
 
 export default Chat;
--- messenger/src/components/chat/message/Message.jsx
+++ messenger/src/components/chat/message/Message.jsx
@@ -1,15 +1,19 @@
 import './Message.css';
 import Proptypes from "prop-types";
 import usersJson from '../../../assets/users.json';
+import { forwardRef } from 'react';
 
-function Message (props) {
+function Message (props, messageRef) {
         
     const container = `container container${props.messageOwner}`
     const messageContainer = `messageContainer message${props.messageOwner}`
 
-    return (
-        <>
-                    <div className={container}>
+
+    // check if the message owner is the user
+    const checkOwner = () => {
+        if (props.messageOwner === "Self") {
+            return (
+                <div className={container} ref={messageRef}>
 
                         <div className={messageContainer}>
                             <p className='messageContent'>
@@ -25,15 +29,49 @@ function Message (props) {
                             })
                         } alt="message owner's icon" className="messageOwnerPfp"/>
 
+                        <p className='messageTime'>{props.messageDate}</p>
+
                     </div>
-                </>
+            );
+        } 
+            else if (typeof(props.messageOwner) === String && props.messageOwner != "Self") {
+                return (
+                    <div className={container} ref={messageRef}>
+
+                        <p className='messageTime'>{props.messageDate}</p>
+
+                        <img src={
+                            usersJson.users.map((user) => {
+                                return user.profile_picture;
+                            })
+                        } alt="message owner's icon" className="messageOwnerPfp"/>
+
+                        <div className={messageContainer}>
+                            <p className='messageContent'>
+                                {
+                                    props.messageContent
+                                }
+                            </p>
+                        </div>
+
+                    </div>
+                );
+            }
+    }
+
+    return (
+        <>
+            {checkOwner()}
+        </>
     );
 }
 
+const MessageComponent = forwardRef(Message);
 
 Message.propTypes = {
     messageContent: Proptypes.string.isRequired,
-    messageOwner: Proptypes.string.isRequired
+    messageOwner: Proptypes.string.isRequired,
+    messageDate: Proptypes.string.isRequired
 }
 
-export default Message;
+export default MessageComponent;
